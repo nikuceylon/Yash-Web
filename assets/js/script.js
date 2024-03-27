f17487dc6ccd39f20f23a42e6ebf576e0c3cd2c6
Niku Ceylon <nikuceylon@gmail.com>
2024-03-27 11:48:56 +0530

    Mobile Nav Added

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$("#hamBtn").click(function(){
+    $("#hamBtn").hide();
+    $("#closeBtn").show();
+  });
+  
+  $("#closeBtn").click(function(){
+    $("#closeBtn").hide();
+    $("#hamBtn").show();
+  });
